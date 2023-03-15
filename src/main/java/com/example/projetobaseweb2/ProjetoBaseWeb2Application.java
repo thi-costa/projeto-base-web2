01044c579d1cf8996a0aea22ae2c14605c912b7a
thi-costa <41833533+thi-costa@users.noreply.github.com>
2023-03-15 20:02:00 -0300

    chore: java spring initial setup

---
+++ src/main/java/com/example/projetobaseweb2/ProjetoBaseWeb2Application.java
@@ -0,0 +1,13 @@
+package com.example.projetobaseweb2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoBaseWeb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoBaseWeb2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projetobaseweb2/ProjetoBaseWeb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetobaseweb2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoBaseWeb2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
