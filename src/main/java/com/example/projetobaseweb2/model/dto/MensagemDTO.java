a94b508d2a4032098b442f0774acde283c64b790
thi-costa <41833533+thi-costa@users.noreply.github.com>
2023-03-22 14:42:50 -0300

    feat: add MensagemDTO

---
+++ src/main/java/com/example/projetobaseweb2/model/dto/MensagemDTO.java
@@ -0,0 +1,12 @@
+package com.example.projetobaseweb2.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class MensagemDTO {
+    private String mensagem;
+}
