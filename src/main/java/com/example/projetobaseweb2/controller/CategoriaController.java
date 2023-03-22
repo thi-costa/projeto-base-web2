7c668efb06f19ba40024ada05d868554862a78c5
thi-costa <41833533+thi-costa@users.noreply.github.com>
2023-03-22 14:43:41 -0300

    feat: add Slf4j for log and try-catch for each route

--- src/main/java/com/example/projetobaseweb2/controller/CategoriaController.java
+++ src/main/java/com/example/projetobaseweb2/controller/CategoriaController.java
@@ -1,43 +1,99 @@
 package com.example.projetobaseweb2.controller;
 
 import com.example.projetobaseweb2.model.dto.CategoriaDTO;
+import com.example.projetobaseweb2.model.dto.MensagemDTO;
 import com.example.projetobaseweb2.service.CategoriaService;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/categorias")
+@Slf4j
 public class CategoriaController {
     @Autowired
     private CategoriaService categoriaService;
 
     @GetMapping
-    public List<CategoriaDTO> listar(){
-        return categoriaService.listar();
+    public ResponseEntity<Object> listar(){
+        try{
+            return ResponseEntity.ok(categoriaService.listar());
+        } catch (Exception ex){
+            log.error(ex.getMessage());
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(new MensagemDTO(ex.getMessage()));
+        }
     }
     @GetMapping("/{id}")
-    public CategoriaDTO pegarCategoriaDTOById(@PathVariable("id") Integer id){
-        return categoriaService.pegarCategoriaDTOById(id);
+    public ResponseEntity<Object> pegarPorId(@PathVariable("id") Integer id){
+        try {
+            return ResponseEntity.ok(categoriaService.pegarPorId(id));
+        } catch ( EntityNotFoundException ex){
+            log.error(ex.getMessage());
+            return ResponseEntity
+                    .status(HttpStatus.NO_CONTENT)
+                    .body(new MensagemDTO(ex.getMessage()));
+        } catch ( Exception ex){
+            log.error(ex.getMessage());
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(new MensagemDTO(ex.getMessage()));
+        }
     }
 
     @PostMapping
-    public CategoriaDTO criar(@RequestBody CategoriaDTO categoriaDTO){
-        return categoriaService.criar(categoriaDTO);
+    public ResponseEntity<Object> criar(@RequestBody CategoriaDTO categoriaDTO){
+        try {
+            return ResponseEntity.status(HttpStatus.CREATED).body(categoriaService.criar(categoriaDTO));
+        } catch (Exception ex){
+            log.error(ex.getMessage());
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(new MensagemDTO(ex.getMessage()));
+        }
     }
 
     @PutMapping("/{id}")
-    public CategoriaDTO editar(
+    public ResponseEntity<Object> editar(
             @RequestBody CategoriaDTO categoriaDTO,
             @PathVariable("id") Integer id){
-        return categoriaService.editar(categoriaDTO, id);
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(categoriaService.editar(categoriaDTO, id));
+        } catch (EntityNotFoundException ex){
+            log.error(ex.getMessage());
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .body(new MensagemDTO(ex.getMessage()));
+        } catch (Exception ex){
+            log.error(ex.getMessage());
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(new MensagemDTO(ex.getMessage()));
+        }
     }
     @DeleteMapping("/{id}")
-    public String deletar(
-            @PathVariable("id") Integer id){
-        categoriaService.deletar(id);
-        return  "Categoria com id " + id + " removido com sucesso!";
+    public ResponseEntity<Object> deletar(
+            @PathVariable("id") Integer id) throws Exception {
+        try {
+            categoriaService.deletar(id);
+            return  ResponseEntity.ok(new MensagemDTO("Categoria com id " + id + " removido com sucesso!"));
+        } catch (EntityNotFoundException ex){
+            log.error(ex.getMessage());
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .body(new MensagemDTO(ex.getMessage()));
+        } catch (Exception ex){
+            log.error(ex.getMessage());
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(new MensagemDTO(ex.getMessage()));
+        }
     }
 
 }
